01ed13085c503bb626c3139911acbf2586ffa1c0
thatseanmoore <spmoore19@gmail.com>
2021-08-25 16:20:23 -0400

    first commit

---
+++ SocialMedia.Models/CommentListItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class CommentListItem
+    {
+        public int Id { get; set; }
+
+        public string CommentText { get; set; }
+
+        [Display(Name = "Date Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Date Last Modified")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
