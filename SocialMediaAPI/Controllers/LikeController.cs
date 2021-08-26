9cdd0fdd332f30d09c974755b43dd2675fa4df85
Jeffrey Hackett <Jeffth92@gmail.com>
2021-08-26 19:58:35 -0400

    Jeff first commit

---
+++ SocialMediaAPI/Controllers/LikeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Identity;
+using SocialMedia.Models;
+using SocialMedia.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocialMediaAPI.Controllers
+{
+    [Authorize]
+    public class LikeController : ApiController
+    {
+        private LikeService CreateLikeService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var likeService = new LikeService(userId);
+            return likeService;
+        }
+        public IHttpActionResult Get(int postId)
+        {
+            LikeService likeService = CreateLikeService();
+            var like = likeService.GetLikesByID(postId);
+            return Ok(like);
+        }
+
+        public IHttpActionResult Post(LikeCreate like)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateLikeService();
+
+            if (!service.CreateLike(like))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
