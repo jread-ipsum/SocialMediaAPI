330dbdb83d8162683449a97a84f5d6d33df0c4f9
Jordan Read <jread6477@gmail.com>
2021-08-25 22:25:35 -0400

    Added ReplyController with Get and Post methods.

---
+++ SocialMediaAPI/Controllers/ReplyController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Identity;
+using SocialMedia.Models.ReplyModels;
+using SocialMedia.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocialMediaAPI.Controllers
+{
+    [Authorize]
+    public class ReplyController : ApiController
+    {
+        public IHttpActionResult Get(int commentId)
+        {
+            ReplyService replyService = CreateReplyService();
+            var replies = replyService.GetRepliesByCommentId(commentId);
+            return Ok(replies);
+        }
+
+        public IHttpActionResult Post(ReplyCreate reply)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateReplyService();
+
+            if (!service.CreateReply(reply))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        private ReplyService CreateReplyService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var replyService = new ReplyService(userId);
+            return replyService;
+        }
+    }
+}
