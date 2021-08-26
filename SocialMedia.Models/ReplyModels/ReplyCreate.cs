0ce1a3108bd903fdc3bd4f90c811371e4634604b
Jordan Read <jread6477@gmail.com>
2021-08-25 21:56:12 -0400

    Added Models: ReplyCreate and ReplyListItem.  Added ReplyService with CreateReply and GetRepliesByCommentId methods.

---
+++ SocialMedia.Models/ReplyModels/ReplyCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models.ReplyModels
+{
+    public class ReplyCreate
+    {
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public int CommentId { get; set; }
+    }
+}
